43c3664fd1b747ff45fa066b13f1df39c954419a
Toth <sandor.toth@hotmail.com>
2021-06-02 21:35:26 +0200

    small refactor

--- src/app/feature/landing/landing-routing.module.ts
+++ src/app/feature/landing/landing-routing.module.ts
@@ -1,7 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-import {NotFoundComponent} from "../../core/not-found/not-found.component";
 import {LandingContainerComponent} from './landing-container/landing-container.component';
 
 const routes: Routes = [
