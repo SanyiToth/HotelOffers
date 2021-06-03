10772af6edaab21a4148128a3062654a41b465cf
Sanyi Tóth <sandor.toth@hotmail.com>
2021-06-03 18:12:12 +0200

    something doesn't work

--- src/app/feature/landing/landing.module.ts
+++ src/app/feature/landing/landing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {LandingRoutingModule} from './landing-routing.module';
 import {TopOffersComponent} from './top-offers/top-offers.component';
+import {LandingRoutingModule} from './landing-routing.module';
 import {CarouselModule} from "primeng/carousel";
+import {LandingContainerComponent} from "./landing-container/landing-container.component";
 
 
 @NgModule({
   declarations: [
+    LandingContainerComponent,
     TopOffersComponent
   ],
   imports: [
