e750eaf535109759c1432e807f41b4bd22d80c04
Sanyi Tóth <sandor.toth@hotmail.com>
2021-06-03 17:27:35 +0200

    top offers component created

--- src/app/feature/landing/landing.module.ts
+++ src/app/feature/landing/landing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
+import { TopOffersComponent } from './landing-container/top-offers/top-offers.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TopOffersComponent
+  ],
   imports: [
     CommonModule,
     LandingRoutingModule
