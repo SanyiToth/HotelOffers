118860da40675053146f5355db6e47c8e09bf31b
Toth <sandor.toth@hotmail.com>
2021-06-06 14:30:31 +0200

    getHotels() and getHotel(id) created

--- src/app/shared/services/hotels/hotels.service.ts
+++ src/app/shared/services/hotels/hotels.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Hotel} from "./hotel.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelsService {
 
-  constructor() { }
+  static API_URL = 'http://localhost:3000'
+  static PATH = "/hotels"
+
+  constructor(private http: HttpClient) {
+  }
+
+  getHotels(): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>(HotelsService.API_URL + HotelsService.PATH);
+  }
+
+  getHotel(id: number): Observable<Hotel> {
+    return this.http.get<Hotel>(HotelsService.API_URL + HotelsService.PATH + '/' + id);
+  }
+
 }
