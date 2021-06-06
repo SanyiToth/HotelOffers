c963981c7241cb12b030e7ebafe375d7e2da9e02
Toth <sandor.toth@hotmail.com>
2021-06-06 10:39:53 +0200

    api url stored in environment.ts

--- src/app/shared/services/offers/offers.service.ts
+++ src/app/shared/services/offers/offers.service.ts
@@ -2,13 +2,13 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Offer} from "./offer.interface";
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OffersService {
 
-  private static API: "http://localhost:3000"
   private static PATH = "/offers"
 
 
@@ -16,11 +16,11 @@ export class OffersService {
   }
 
   getOffers(limit: number = 5): Observable<Offer[]> {
-    return this.http.get<Offer[]>(OffersService.API + OffersService.PATH + '?_limit=' + limit);
+    return this.http.get<Offer[]>(environment.API + OffersService.PATH + '?_limit=' + limit);
   }
 
   getOffer(id: number): Observable<Offer> {
-    return this.http.get<Offer>(OffersService.API + OffersService.PATH + '/' + id)
+    return this.http.get<Offer>(environment.API + OffersService.PATH + '/' + id)
   }
 
 }
