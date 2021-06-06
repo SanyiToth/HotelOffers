4596d5c609331893182a085ea7f0e601b3a43b64
Toth <sandor.toth@hotmail.com>
2021-06-06 14:07:26 +0200

    update hotel interface

--- src/app/shared/services/hotels/hotel.interface.ts
+++ src/app/shared/services/hotels/hotel.interface.ts
@@ -2,7 +2,12 @@ export interface Hotel {
   hotelId: number;
   id: number;
   name: string;
-  address: string;
+  address: {
+    country?: string;
+    city: string;
+    street: string;
+    postCode: number;
+  };
   classification: string;
   email: string;
   phone: string;
