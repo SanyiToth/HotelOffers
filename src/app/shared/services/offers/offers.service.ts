b9e0981367a33d909bc940490b78c012f90b2d23
Toth <sandor.toth@hotmail.com>
2021-06-06 09:55:23 +0200

    offers service has been changed

--- src/app/shared/services/offers/offers.service.ts
+++ src/app/shared/services/offers/offers.service.ts
@@ -2,24 +2,25 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Offer} from "./offer.interface";
-import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OffersService {
 
+  private static API: "http://localhost:3000"
   private static PATH = "/offers"
+
 
   constructor(private http: HttpClient) {
   }
 
   getOffers(limit: number = 5): Observable<Offer[]> {
-    return this.http.get<Offer[]>(environment.API + OffersService.PATH + '?_limit=' + limit);
+    return this.http.get<Offer[]>(OffersService.API + OffersService.PATH + '?_limit=' + limit);
   }
 
   getOffer(id: number): Observable<Offer> {
-    return this.http.get<Offer>(environment.API + OffersService.PATH + '/' + id)
+    return this.http.get<Offer>(OffersService.API + OffersService.PATH + '/' + id)
   }
 
 }
