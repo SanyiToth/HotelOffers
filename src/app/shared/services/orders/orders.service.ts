0cc7f289bc6e8ebe51da375f376c65cbb51adf2f
Toth <sandor.toth@hotmail.com>
2021-06-06 17:29:47 +0200

    getOrders() and getOrder(id) created

--- src/app/shared/services/orders/orders.service.ts
+++ src/app/shared/services/orders/orders.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
 
-  constructor() { }
+  static API_URL = 'http://localhost:3000'
+  static PATH = "/orders"
+
+  constructor(private http: HttpClient) {
+  }
+
+  getOffers(limit: number = 5): Observable<any[]> {
+    return this.http.get<any[]>(OrdersService.API_URL + OrdersService.PATH);
+  }
+
+  getOffer(id: number): Observable<any> {
+    return this.http.get<any>(OrdersService.API_URL + OrdersService.PATH + '/' + id)
+  }
 }
