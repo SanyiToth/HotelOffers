334d3193e8ae11b48d51766dcde96969f6974cdd
Sanyi Tóth <sandor.toth@hotmail.com>
2021-06-08 22:16:17 +0200

    refactro

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ReviewsService} from "../services/reviews/reviews.service";
-import {Review} from "../services/reviews/review.interface";
+import {Rating} from "./rating.interface";
+
 
 @Component({
   selector: 'app-rating',
@@ -8,30 +8,14 @@ import {Review} from "../services/reviews/review.interface";
   styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent implements OnInit {
-  @Input() hotelId: number = 0;
-  reviews: Review[] = []
-  rating: string = '';
-  numberOfRatings: number = 0;
+  @Input() data: Rating | undefined;
 
-  constructor(private reviewsService: ReviewsService) {
-  }
-
-  avgRating(reviews: Review[]): string {
-    let sum: number = 0;
-    reviews.forEach((item) => {
-        sum = sum + item.rating;
-    });
-    return (sum / reviews.length).toFixed(1);
+  constructor() {
   }
 
 
   ngOnInit(): void {
-    this.reviewsService.getReviews(this.hotelId)
-      .subscribe(reviews => {
-        this.reviews = reviews;
-        this.rating = this.avgRating(reviews)
-        this.numberOfRatings = reviews.length;
-      })
+
   }
 
 }
--- src/app/shared/services/reviews/review.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface Review {
-  hotelId: number;
-  rating: number;
-}
