181ffa963ced67032bce9d5dcec3b8fc88be0739
Sanyi Tóth <sandor.toth@hotmail.com>
2021-06-09 19:47:26 +0200

    dashboard component moved to dashboard-container directory

--- src/app/feature/dashboard/dashboard-routing.module.ts
+++ src/app/feature/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
-import {DashboardComponent} from "./dashboard.component";
+import {DashboardComponent} from "./dashboard-container/dashboard.component";
 
 const routes: Routes = [
   {path: '', component: DashboardComponent}
