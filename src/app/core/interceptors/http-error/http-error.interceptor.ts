9548cdbad6c0a02bdf251d7aed852d92ea049759
Sanyi Tóth <sandor.toth@hotmail.com>
2021-06-10 16:16:21 +0200

    basic error response Ooops! Something happened, try again later!

--- src/app/core/interceptors/http-error/http-error.interceptor.ts
+++ src/app/core/interceptors/http-error/http-error.interceptor.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor, HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Observable, throwError} from 'rxjs';
+import {catchError, retry} from 'rxjs/operators';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() {
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+    return next.handle(request).pipe(
+      retry(1),
+      catchError(this.handleError.bind(this))
+    );
   }
+
+  handleError(error: HttpErrorResponse): Observable<HttpEvent<Error>> {
+    const errorMessage = 'Ooops! Something happened, try again later! ';
+    return throwError(new Error(errorMessage));
+  }
+
+
 }
