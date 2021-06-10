3b882d7bcf3c8d4be190b377dccc1cddcf777493
Sanyi Tóth <sandor.toth@hotmail.com>
2021-06-10 18:18:02 +0200

    HTTP_INTERCEPTORS and AuthInterceptor imported in auth.module

--- src/app/shared/auth/auth.module.ts
+++ src/app/shared/auth/auth.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthGuard} from "./auth.guard";
 import {AuthService} from "./auth.service";
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {AuthInterceptor} from "./auth.interceptor";
 
 
 @NgModule({
@@ -13,6 +15,11 @@ import {AuthService} from "./auth.service";
   providers: [
     AuthService,
     AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
   ]
 })
 export class AuthModule {
