2653ca6775110b16560c5779f1d971b3e5bdd33d
Sanyi Tóth <sandor.toth@hotmail.com>
2021-06-10 18:43:13 +0200

    auth service complete

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {AccessToken, LoginCredential, User} from './auth.interface';
+import {HttpClient} from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+import {environment} from "../../../environments/environment.prod";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private static PATH = '/login';
+
+  public get currentJwtValue(): AccessToken {
+    return this.jwtSubject.value;
+  }
+
+  constructor(private http: HttpClient) {
+    const jwtToken = JSON.parse(localStorage.getItem('jwt') as string) as AccessToken || undefined;
+    this.jwtSubject = new BehaviorSubject<AccessToken>(jwtToken);
+  }
+
+  jwtSubject: BehaviorSubject<AccessToken> = new BehaviorSubject<AccessToken>(null);
+
+
+  login(credentials: LoginCredential): Observable<AccessToken> {
+    return this.http
+      .post<AccessToken>(environment.API_URL + AuthService.PATH, credentials)
+      .pipe(
+        tap(token => {
+          localStorage.setItem('jwt', JSON.stringify(token));
+          this.jwtSubject.next(token);
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.setItem('jwt', JSON.stringify(''));
+    this.jwtSubject.next(null);
+  }
+
+
+  isLoggedIn(): boolean {
+    console.log('this.currentJwtValue', this.currentJwtValue);
+    return !!this.jwtSubject.value;
+  }
+
+
 }
