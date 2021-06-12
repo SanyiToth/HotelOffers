923a5e221d461837dda99a6cf0a30fce73f8c36b
fumate <mate.fuller@gmail.com>
2021-06-12 11:34:34 +0200

    added corrections

--- src/app/feature/results/result-item-list/result-item-list.component.ts
+++ src/app/feature/results/result-item-list/result-item-list.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { OffersService } from "../../../shared/services/offers/offers.service";
+import { Component, Input, OnInit } from '@angular/core';
 import { Offer } from "../../../shared/services/offers/offer.interface";
 
 @Component({
   selector: 'app-result-item-list',
   templateUrl: './result-item-list.component.html',
-  styleUrls: ['./result-item-list.component.css']
+  styleUrls: ['./result-item-list.component.scss']
 })
 export class ResultItemListComponent implements OnInit {
 
-  public items: Offer[] = [];
+@Input()
+items: Offer[] = [];
 
-  constructor(private item: OffersService) {}
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.item.getOffers().subscribe(data => {
-      this.items = data;
-    });
   }
 }
