3c1bf19a0f3db0e941364ebb845ce1c9e7cb7788
SanyiToth <sandor.toth@hotmail.com>
2021-06-14 13:56:26 +0200

    routName feld created

--- src/app/feature/dashboard/dashboard-container/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard-container/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  routeName: string;
 
-  constructor() { }
+  constructor() {
+    this.routeName = "-Dashboard"
+  }
 
   ngOnInit(): void {
   }
