299e6648ae22f422db93ba359112c5edee4145f2
SanyiToth <sandor.toth@hotmail.com>
2021-06-14 13:49:21 +0200

    dashboard header component deleted

--- src/app/feature/dashboard/dashboard.module.ts
+++ src/app/feature/dashboard/dashboard.module.ts
@@ -2,14 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard-container/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 import {MatIconModule} from "@angular/material/icon";
 import {HeaderModule} from "../../shared/header/header.module";
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    DashboardHeaderComponent
   ],
   imports: [
     CommonModule,
