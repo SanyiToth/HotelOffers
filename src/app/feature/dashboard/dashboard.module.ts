77713a051049564ed256f002ff7f30d928b14350
SanyiToth <sandor.toth@hotmail.com>
2021-06-14 10:21:19 +0200

    dashboard-header component generated

--- src/app/feature/dashboard/dashboard.module.ts
+++ src/app/feature/dashboard/dashboard.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard-container/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardHeaderComponent
   ],
   imports: [
     CommonModule,
