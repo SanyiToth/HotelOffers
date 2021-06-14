812c5d419b8f38bbdbd134e2e014ccc6fd279325
SanyiToth <sandor.toth@hotmail.com>
2021-06-14 13:48:40 +0200

    app header in dashboard component works with a content projection

--- src/app/feature/dashboard/dashboard.module.ts
+++ src/app/feature/dashboard/dashboard.module.ts
@@ -3,6 +3,8 @@ import {CommonModule} from '@angular/common';
 import {DashboardComponent} from './dashboard-container/dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
+import {MatIconModule} from "@angular/material/icon";
+import {HeaderModule} from "../../shared/header/header.module";
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.co
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    HeaderModule,
+    DashboardRoutingModule,
+    MatIconModule
   ],
   exports: [DashboardComponent]
 })
