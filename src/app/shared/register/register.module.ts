67734ca45b97d4342369914ccab00f9b91c52d8e
Mark Matuza <matuzamark@gmail.com>
2021-06-18 13:48:00 +0200

    corrections on register component, module

--- src/app/shared/register/register.module.ts
+++ src/app/shared/register/register.module.ts
@@ -15,7 +15,6 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   imports: [
     CommonModule,
-    FormsModule,
     RegisterRoutingModule,
     ReactiveFormsModule,
     MatInputModule,
