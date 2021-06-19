3046a26346db20647367973a169e6f451fec8f04
SanyiToth <sandor.toth@hotmail.com>
2021-06-19 20:23:01 +0200

    navigate works

--- src/app/feature/results/result-item/result-item.component.ts
+++ src/app/feature/results/result-item/result-item.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {switchMap, tap} from "rxjs/operators";
+import {OffersService} from "../../../shared/services/offers/offers.service";
 
 
 @Component({
@@ -7,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./result-item.component.scss']
 })
 export class ResultItemComponent implements OnInit {
+  offer: any;
+  id: any;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private offersService: OffersService) {
+  }
 
   ngOnInit(): void {
+
+    this.route.params
+      .pipe(
+        tap(params => {
+            this.id = params.id;
+          }
+        ),
+        switchMap(() => this.offersService.getOffer(this.id)))
+      .subscribe(response => {
+        this.offer = response;
+        console.log('offer',this.offer)
+      }, error => {
+        this.errorMessage = error;
+      });
   }
 
 }
