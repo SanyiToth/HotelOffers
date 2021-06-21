48e3bbaf8e9c29d712bdbf1aa48d7caaf568dd78
szbRebeka <szrebeka94@gmail.com>
2021-06-21 09:40:49 +0200

    create item card base

---
+++ src/app/feature/dashboard/dashboard-offers/dashboard-offer-item/dashboard-offer-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-offer-item',
+  templateUrl: './dashboard-offer-item.component.html',
+  styleUrls: ['./dashboard-offer-item.component.scss']
+})
+export class DashboardOfferItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/feature/dashboard/dashboard-offers/dashboard-offers.module.ts
+++ src/app/feature/dashboard/dashboard-offers/dashboard-offers.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardOffersContainerComponent } from "./dashboard-offers-container/dashboard-offers-container.component";
 import { DashboardOffersRoutingModule  } from "./dashboard-offers-routing.module";
+import { DashboardOfferItemComponent } from './dashboard-offer-item/dashboard-offer-item.component';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  declarations: [DashboardOffersContainerComponent],
+  declarations: [DashboardOffersContainerComponent, DashboardOfferItemComponent],
   imports: [
     CommonModule,
-    DashboardOffersRoutingModule
+    DashboardOffersRoutingModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class DashboardOffersModule { }
