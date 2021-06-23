b19ba827d5c4159ef9faf8a25519927cb043ead6
SanyiToth <sandor.toth@hotmail.com>
2021-06-23 20:10:05 +0200

    secondform group created

--- src/app/feature/dashboard/dashboard-new-offer/dashboard-new-offer.component.ts
+++ src/app/feature/dashboard/dashboard-new-offer/dashboard-new-offer.component.ts
@@ -1,10 +1,18 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {
+  AbstractControl,
+  AbstractFormGroupDirective, FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
 import {Observable} from "rxjs";
 import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {MatAutocomplete, MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 import {map, startWith} from "rxjs/operators";
 import {MatChipInputEvent} from "@angular/material/chips";
+import {MatCheckboxChange} from "@angular/material/checkbox";
 
 
 @Component({
@@ -22,9 +30,7 @@ export class DashboardNewOfferComponent {
   filteredTags: Observable<string[]> | undefined;
   tagsArray: string[];
   allTags: string[] = ['Wifi', 'Free parking', 'Swimming pool', 'Beachfront', 'All inclusive', 'Breakfast included'];
-  paymentOptions = ['Cash', 'Bank card', 'Bank transfer', 'Visa', 'American express', 'Master Card', 'Szép kártya']
-
-  firstFormGroup: FormGroup;
+  payArr: string[] = [];
 
 
   // @ts-ignore
@@ -32,25 +38,57 @@ export class DashboardNewOfferComponent {
   // @ts-ignore
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(private fb: FormBuilder) {
 
 
-    this.firstFormGroup = this._formBuilder.group({
+    this.firstFormGroup = this.fb.group({
       heading: ['', Validators.required],
       details: ['', [Validators.required, Validators.maxLength(50)]],
       startDate: ['', Validators.required],
       endDate: ['', Validators.required],
       availableOffers: [{value: '', disabled: false}, Validators.required],
-      price: ['', Validators.required],
-      description: ['', [Validators.required, Validators.maxLength(300)]],
-      tags: [['Wifi', 'Free parking'], [Validators.required]],
-      paymentMethods: ['', [Validators.required]]
+      price: ['', Validators.required]
+
     });
+
+    this.secondFormGroup = this.fb.group({
+      description: ['', [Validators.required, Validators.maxLength(300)]],
+      tags: [['Wifi'], [Validators.required]],
+      paymentMethods: this.fb.group({
+        cash: false,
+        bankCard: false,
+        bankTransfer: false,
+        szepKartya: false,
+      })
+    })
+
+
     this.tagsArray = this.tags?.value
     this.filteredTags = this.tagCtrl.valueChanges.pipe(
       startWith(null),
-      map((fruit: string | null) => fruit ? this._filter(fruit) : this.allTags.slice()));
+      map((tag: string | null) => tag ? this._filter(tag) : this.allTags.slice()));
+  }
+
+
+  onSubmit() {
+    console.log(this.firstFormGroup.value)
+    console.log(this.secondFormGroup.value)
+  }
+
+  onChange() {
+    if (this.availableOffers?.disabled) {
+      this.availableOffers.enable();
+      this.availableOffers?.reset();
+    } else {
+      this.availableOffers?.disable();
+      this.availableOffers?.reset();
+    }
+  }
+
+  ngOnInit() {
   }
 
   add(event: MatChipInputEvent): void {
@@ -59,13 +97,11 @@ export class DashboardNewOfferComponent {
       this.tagsArray.push(value);
     }
     event.chipInput!.clear();
-
     this.tagCtrl.setValue(null);
   }
 
   remove(fruit: string): void {
     const index = this.tagsArray.indexOf(fruit);
-
     if (index >= 0) {
       this.tagsArray.splice(index, 1);
     }
@@ -79,27 +115,7 @@ export class DashboardNewOfferComponent {
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-
-    return this.allTags.filter(fruit => fruit.toLowerCase().indexOf(filterValue) === 0);
-  }
-
-
-  ngOnInit() {
-    console.log(this.tags?.value)
-  }
-
-  onSubmit() {
-    console.log(this.firstFormGroup.value)
-  }
-
-  onChange() {
-    if (this.availableOffers?.disabled) {
-      this.availableOffers.enable();
-      this.availableOffers?.reset();
-    } else {
-      this.availableOffers?.disable();
-      this.availableOffers?.reset();
-    }
+    return this.allTags.filter(tag => tag.toLowerCase().indexOf(filterValue) === 0);
   }
 
   get heading(): AbstractControl | null {
@@ -127,10 +143,14 @@ export class DashboardNewOfferComponent {
   }
 
   get description(): AbstractControl | null {
-    return this.firstFormGroup.get('description');
+    return this.secondFormGroup.get('description');
   }
 
   get tags(): AbstractControl | null {
-    return this.firstFormGroup.get('tags');
+    return this.secondFormGroup.get('tags');
+  }
+
+  get paymentMethods(): any {
+    return this.secondFormGroup.get('paymentMethods')
   }
 }
