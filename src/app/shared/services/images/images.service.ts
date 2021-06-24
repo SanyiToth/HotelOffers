8755cc2fc6215ab7366789e31a4de40dc916c939
SanyiToth <sandor.toth@hotmail.com>
2021-06-24 22:14:15 +0200

    uploadImage method created in images service

--- src/app/shared/services/images/images.service.ts
+++ src/app/shared/services/images/images.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImagesService {
 
-  constructor() { }
+  private readonly IMGUR_UPLOAD_URL = 'https://api.imgur.com/3/image';
+  private clientId: string = '5999e2155ec5874';
+
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  upLoadImage(image: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Client-ID ${this.clientId}`
+      }),
+    };
+    const formData = new FormData();
+    formData.append('image', image);
+    return this.http.post(`${this.IMGUR_UPLOAD_URL}`, formData, httpOptions);
+  }
 }
