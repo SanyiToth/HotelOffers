538696028f4ccfccce3b4421d91984828e2c23c3
SanyiToth <sandor.toth@hotmail.com>
2021-06-26 12:56:15 +0200

    isLinear = true

--- src/app/feature/dashboard/dashboard-new-offer/dashboard-new-offer.component.ts
+++ src/app/feature/dashboard/dashboard-new-offer/dashboard-new-offer.component.ts
@@ -33,7 +33,7 @@ export class DashboardNewOfferComponent {
   secondFormGroup: FormGroup;
 
   //mat-stepper
-  isLinear = false;
+  isLinear = true;
 
   //firstFormGroup Validators Magic Numbers
   static readonly DETAILS_MAX_LENGTH = 50;
