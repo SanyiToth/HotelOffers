f7fb1a78ff9997d8129b8883faf0a7c8e17c50c4
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-06-28 13:16:05 +0200

    fix env import

--- src/app/shared/services/hotels/hotels.service.ts
+++ src/app/shared/services/hotels/hotels.service.ts
@@ -2,17 +2,16 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Hotel} from "./hotel.interface";
-import {environment} from "../../../../environments/environment.prod";
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelsService {
 
-  static PATH = "/hotels"
+  static PATH = "/providers"
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getHotels(): Observable<Hotel[]> {
     return this.http.get<Hotel[]>(environment.API_URL + HotelsService.PATH);
