e5ffb7777833d5c586d8b4a3e1d95f8079632857
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-06-29 12:13:59 +0200

    add new production backend URL (heroku)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://hotel-offers1-backend.herokuapp.com/api/',
   production: true,
   OFFER_EXTRAS : ['Wifi', 'Free parking', 'Swimming pool', 'Beachfront', 'All inclusive', 'Breakfast included']
 };
