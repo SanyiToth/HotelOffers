b7c2436f7f33a0381b2e6c49d53dbd2fbf93e6ee
Toth <sandor.toth@hotmail.com>
2021-06-30 19:35:26 +0200

    environment.prod channged

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://hotel-offers1-backend.herokuapp.com',
+  API_URL: 'http://localhost:3000',
   OFFER_EXTRAS : ['Wifi', 'Free parking', 'Swimming pool', 'Beachfront', 'All inclusive', 'Breakfast included']
 };
