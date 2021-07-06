40da00971f4a12f5d93725703eb6b43b63609cda
szbRebeka <szrebeka94@gmail.com>
2021-07-06 09:13:49 +0200

    added date pipe to offer-item

--- src/app/feature/dashboard/dashboard-offers/dashboard-offers.service.ts
+++ src/app/feature/dashboard/dashboard-offers/dashboard-offers.service.ts
@@ -1,9 +1,8 @@
-import {Injectable} from '@angular/core';
-import {Observable, throwError} from "rxjs";
-import {Offer} from "../../../shared/services/offers/offer.interface";
-import {environment} from "../../../../environments/environment.prod";
-import {HttpClient, HttpErrorResponse, HttpParams} from "@angular/common/http";
-import {catchError, retry} from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { Observable } from "rxjs";
+import { Offer } from "../../../shared/services/offers/offer.interface";
+import { environment } from "../../../../environments/environment.prod";
+import { HttpClient,HttpParams } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +12,12 @@ export class DashboardOffersService {
   static PATH = "/offers/findByProviderId";
 
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getOffersByProviderId(providerId: string, status?: string): Observable<Offer[]> {
     console.log("status from service", status)
-    //if-else az allra
     if (status) {
       return this.http.get<Offer[]>(environment.API_URL + DashboardOffersService.PATH + '/' + providerId, {
-        // @ts-ignore
         params: new HttpParams().set('status', status)
       })
     } else {
