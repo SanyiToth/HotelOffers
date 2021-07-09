133d74679c895a18d3f1d979eee06b5d11786e39
SanyiToth <sandor.toth@hotmail.com>
2021-07-09 11:25:24 +0200

    hotel and hotel stars displayed in details

--- src/app/feature/results/result-item/result-item.component.ts
+++ src/app/feature/results/result-item/result-item.component.ts
@@ -1,74 +1,86 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {switchMap, tap} from "rxjs/operators";
 import {OffersService} from "../../../shared/services/offers/offers.service";
 import {Offer} from "../../../shared/services/offers/offer.interface";
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { OrderComponent } from 'src/app/shared/orders/order/order.component';
+import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
+import {OrderComponent} from 'src/app/shared/orders/order/order.component';
 
 
 @Component({
-  selector: 'app-result-item',
-  templateUrl: './result-item.component.html',
-  styleUrls: ['./result-item.component.scss']
+    selector: 'app-result-item',
+    templateUrl: './result-item.component.html',
+    styleUrls: ['./result-item.component.scss']
 })
-export class ResultItemComponent implements OnInit {
-  offer!: Offer;
-  id: number | undefined;
-  errorMessage = '';
-  displayBasic!: boolean;
+export class ResultItemComponent implements OnInit, AfterViewInit {
+    offer!: Offer;
+    id!: number;
+    errorMessage = '';
+    displayBasic!: boolean;
+    stars: string[]=[];
 
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ];
 
-  responsiveOptions:any[] = [
-    {
-      breakpoint: '1024px',
-      numVisible: 5
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 3
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1
+    constructor(private route: ActivatedRoute, private offersService: OffersService, private dialog: MatDialog) {
     }
-  ];
 
-  constructor(private route: ActivatedRoute, private offersService: OffersService, private dialog: MatDialog) {
-  }
+    ngOnInit(): void {
+        this.route.params
+            .pipe(
+                tap(params => {
+                        this.id = params.id;
+                    }
+                ),
+                switchMap(() => this.offersService.getOffer(this.id)))
+            .subscribe(response => {
+                this.offer = response;
+            }, error => {
+                this.errorMessage = error;
+            });
+    }
 
-  ngOnInit(): void {
-    this.route.params
-      .pipe(
-        tap(params => {
-            this.id = params.id;
-          }
-        ),
-        switchMap(() => this.offersService.getOffer(this.id)))
-      .subscribe(response => {
-        this.offer = response;
-      }, error => {
-        this.errorMessage = error;
-      });
-  }
 
-  openDialog() {
+    getStars(): string[] {
+        for (let i = 1; i <= this.offer?.provider?.classification; i++) {
+            this.stars.push('star');
+        }
+        return this.stars;
+    }
 
-    const dialogConfig = new MatDialogConfig();
+    openDialog() {
 
-    dialogConfig.disableClose = false;
-    dialogConfig.autoFocus = true;
-    dialogConfig.hasBackdrop = false;
-    dialogConfig.maxWidth = "60vw";
-    dialogConfig.width = '100%';
-    const dialogRef = this.dialog.open(OrderComponent, dialogConfig);
+        const dialogConfig = new MatDialogConfig();
 
-    dialogRef.afterClosed().subscribe(
-      data => {
-        console.log(data);
-      }
-    )
+        dialogConfig.disableClose = false;
+        dialogConfig.autoFocus = true;
+        dialogConfig.hasBackdrop = false;
+        dialogConfig.maxWidth = "60vw";
+        dialogConfig.width = '100%';
+        const dialogRef = this.dialog.open(OrderComponent, dialogConfig);
 
-  }
+        dialogRef.afterClosed().subscribe(
+            data => {
+                console.log(data);
+            }
+        )
+
+    }
+
+    ngAfterViewInit(): void {
+        console.log(this.getStars());
+    }
 
 }
