5844348499b95f8ac8b29e2a143781fbf6661b4d
szbRebeka <szrebeka94@gmail.com>
2021-07-09 15:01:32 +0200

    created directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { TermsAndConditionsComponent } from './shared/pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './shared/pages/contact-us/contact-us.component';
+import { FaqComponent } from './shared/pages/faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent,    
+    AppComponent,
+    TermsAndConditionsComponent,
+    ContactUsComponent,
+    FaqComponent,    
   ],
   imports: [
     BrowserModule,
