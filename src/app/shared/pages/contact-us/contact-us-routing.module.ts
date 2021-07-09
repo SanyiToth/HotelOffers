6b103f2a5b9afa5b75ab8579d4b826047bc35682
szbRebeka <szrebeka94@gmail.com>
2021-07-09 17:08:05 +0200

    routing works

--- src/app/shared/pages/contact-us/contact-us-routing.module.ts
+++ src/app/shared/pages/contact-us/contact-us-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ContactUsComponent} from "./contact-us.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ContactUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/pages/terms-and-conditions/terms-and-conditions-routing.module.ts
+++ src/app/shared/pages/terms-and-conditions/terms-and-conditions-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TermsAndConditionsComponent} from "./terms-and-conditions.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: TermsAndConditionsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
