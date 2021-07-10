54ebc39e392931b790ec1d9e6d529d5a711b6973
SanyiToth <sandor.toth@hotmail.com>
2021-07-10 09:37:28 +0200

    bug fixed

--- src/app/feature/results/result-item/result-item.component.ts
+++ src/app/feature/results/result-item/result-item.component.ts
@@ -12,7 +12,7 @@ import {OrderComponent} from 'src/app/shared/orders/order/order.component';
   templateUrl: './result-item.component.html',
   styleUrls: ['./result-item.component.scss']
 })
-export class ResultItemComponent implements OnInit {
+export class ResultItemComponent implements OnInit,AfterViewInit {
   offer!: Offer;
   id!: number;
   errorMessage = '';
@@ -76,6 +76,10 @@ export class ResultItemComponent implements OnInit {
         console.log(data);
       }
     )
+  }
+
+  ngAfterViewInit(): void {
+    this.getStars();
   }
 
 
