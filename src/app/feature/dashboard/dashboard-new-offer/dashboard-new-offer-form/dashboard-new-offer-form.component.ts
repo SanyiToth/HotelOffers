8038a113c8bc6ae0142759984106a7bb136323d6
SanyiToth <sandor.toth@hotmail.com>
2021-07-17 13:25:42 +0200

    dashboard new offer form generated

---
+++ src/app/feature/dashboard/dashboard-new-offer/dashboard-new-offer-form/dashboard-new-offer-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-new-offer-form',
+  templateUrl: './dashboard-new-offer-form.component.html',
+  styleUrls: ['./dashboard-new-offer-form.component.css']
+})
+export class DashboardNewOfferFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
