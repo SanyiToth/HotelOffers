bb621372aefc5f2e3541d433d83609a62ce1d7b7
SanyiToth <sandor.toth@hotmail.com>
2021-07-21 16:15:05 +0200

    env prod url has been changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'http://13.58.199.34:3000',
+  API_URL: 'https://nodejs.firstwebpage.hu',
   OFFER_EXTRAS: ['Wifi', 'Free parking', 'Swimming pool', 'Beachfront', 'All inclusive', 'Breakfast included']
 };
